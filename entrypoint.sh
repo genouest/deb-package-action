889fdee700fb0ba0a3907690d3acec82b830f62a
Olivier Sallou <olivier.sallou@irisa.fr>
2021-11-15 16:49:58 +0100

    fix build

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,26 @@
 set -e
 
 apt-get update && apt-get -y install devscripts git-buildpackage
+sourceversion=`dpkg-parsechangelog --show-field Version | cut -d'-' -f1`
 version=`dpkg-parsechangelog --show-field Version`
+package=`dpkg-parsechangelog --show-field Source`
+
 
 mk-build-deps --install --tool='apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends --yes' debian/control
+
 mkdir -p ../build-area
+
+echo "create source tarball"
+mkdir -p /tmp/${package}_${sourceversion}.orig
+cp -a . /tmp/${package}_${sourceversion}.orig/
+rm -rf /tmp/${package}_${sourceversion}.orig/.git
+rm -rf /tmp/${package}_${sourceversion}.orig/debian
+tar cvfz ../build-area/${package}_${sourceversion}.orig.tar.gz -C /tmp ${package}_${sourceversion}.orig
+rm -rf /tmp/${package}_${sourceversion}.orig
+
+echo "build package"
+rm *.deb *.buildinfo *.changes
+
 echo y | gbp buildpackage --git-ignore-branch --no-sign --git-export-dir=../build-area --git-no-create-orig
 cp ../build-area/*.deb ./
 ls -l *.deb
