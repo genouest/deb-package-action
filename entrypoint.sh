d538593a4ac001d6d3dd0dd56faaccf285dc7f4f
Olivier Sallou <olivier.sallou@irisa.fr>
2021-11-15 16:18:52 +0100

    cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,6 @@ version=`dpkg-parsechangelog --show-field Version`
 
 mk-build-deps --install --tool='apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends --yes' debian/control
 mkdir -p ../build-area
-rm ../build-area/*.deb
 echo y | gbp buildpackage --git-ignore-branch --no-sign --git-export-dir=../build-area --git-no-create-orig
 cp ../build-area/*.deb ./
 ls -l *.deb
